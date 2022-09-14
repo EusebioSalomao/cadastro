79f13ef18278f6008547b6b545c697dbed7d5036
Eusébio Salomão <eusebiosalomao@outlook.com>
2022-09-14 18:22:09 -0300

    Automatização do cadastro dinamico

--- funcao.js
+++ funcao.js
@@ -1,4 +1,16 @@
 function inserir(){
     nome = $("#nome").val();
-alert(nome)
+    bilhete = $("#bilhete").val();
+    telefone = $("#telefone").val();
+    bairro = $("#bairro").val();
+
+    if(nome =="" || bilhete ==""){
+        alert("Por favor preencha correctamente os campos!")
+    }else{
+
+    dadoPraTabela = "";
+    dadoPraTabela += "<tr><td>"+nome+"</td><td>"+bairro+"</td><td>"+bilhete+"</td><td>"+telefone+"</td></tr>";
+    document.getElementById("cadastro").innerHTML += dadoPraTabela;
+    $(".campos").val("")
+}
 }
